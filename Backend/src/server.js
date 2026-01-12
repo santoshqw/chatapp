0794904e019f19d0afa2f09c2695f7fc112e64c0
Santosh Majakoti <santoshjr08@gmail.com>
2026-01-12 10:42:18 +0545

    intalled vite ,express ,tailwind and db connection with atlas

---
+++ Backend/src/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express =require('express');
+const app =express();
+const connectDB =require("./config/connectDB");
+
+const PORT =process.env.PORT;
+
+connectDB();
+
+app.listen(PORT,(req,res)=>{
+    console.log(`server is running at port:${PORT}`);
+});
+
---
+++ Frontend/myapp/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-white bg-red-200'>App</div>
+  )
+}
+
+export default App
